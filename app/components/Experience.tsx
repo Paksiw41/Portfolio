7e38a8cfed36a6426f8fb614baeac544e32ed948
Paksiw41 <mncursal.addu.edu.ph>
2025-04-25 16:19:45 +0800

    attempt 3, fix white bg on about me section on ipad

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -28,7 +28,7 @@ export default function Experience() {
   return (
     <section
       id="experience"
-      className="min-h-screen w-full bg-pimary text-[var(--color-text-light)] flex items-center justify-center px-4 py-16"
+      className="min-h-screen w-full bg-primary text-[var(--color-text-light)] flex items-center justify-center px-4 py-16"
     >
       <motion.div
         className="max-w-5xl w-full space-y-12"
