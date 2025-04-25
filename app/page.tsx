c98c639061b3330c3659b1badca411a2449bec07
Paksiw41 <mncursal.addu.edu.ph>
2025-04-25 14:27:49 +0800

    add a roboto fonts

--- app/page.tsx
+++ app/page.tsx
@@ -87,7 +87,7 @@ export default function Home() {
             <div className="bg-gray-800 p-6 rounded-xl shadow-md">
               <h3 className="text-[1.5vw] font-bold text-[#18c79b]">Web Developer – Major Subject Project</h3>
               <p className="mt-2 text-[1vw]">MMML Recruitment Services Website — 2024</p>
-              <ul className="mt-4 list-disc pl-6 text-[1vw] space-y-2">
+              <ul className="mt-5 list-disc pl-6 text-[1vw] space-y-2">
                 <li>Built a recruitment platform to simplify job applications and client management.</li>
                 <li>Developed both the frontend (React.js) and backend (Node.js/Express), with MySQL for the database.</li>
                 <li>Collaborated with classmates to plan and implement user-friendly workflows and features.</li>
@@ -99,7 +99,7 @@ export default function Home() {
             <div className="bg-gray-800 p-6 rounded-xl shadow-md">
               <h3 className="text-[1.5vw] font-bold text-[#18c79b]">Personal Portfolio Website</h3>
               <p className="mt-2 text-[1vw]">Self-initiated Project — 2024</p>
-              <ul className="mt-4 list-disc pl-6 text-[1vw] space-y-2">
+              <ul className="mt-5 list-disc pl-6 text-[1vw] space-y-2">
                 <li>Designed and developed a personal website to showcase my skills, projects, and contact information.</li>
                 <li>Built using React.js and Tailwind CSS; deployed on Vercel for easy sharing and accessibility.</li>
                 <li>Enhanced skills in responsive design, UI/UX, and version control using GitHub.</li>
